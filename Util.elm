804a58e4933b1721f83acd81e1bc5f01000d6be3
Anwesh43 <anweshthecool0@gmail.com>
2017-09-02 20:04:23 +0530

    adding payload to the Answer type

--- Util.elm
+++ Util.elm
@@ -12,8 +12,9 @@ caseStr a = case a of
         1 -> "hello"
         2 -> "world"
         _ -> "none"
-type Answer =   YES|NO
+type Answer =   YES|NO|OTHER String
 respond : Answer -> String
 respond answer = case answer of
               YES -> "the response is yes"
               NO -> "the response is no"
+              OTHER resp-> "answer is " ++ " "++resp
